d7f7879487d287ef37e2440c3572bddf2dd9a1cf
ilyabrilev <ilyabrilevirk@yandex.ru>
2024-04-02 18:53:42 +0300

    regex to channels

--- config.py
+++ config.py
@@ -1,4 +1,16 @@
 from decouple import config
+import logging
+
+class ChannelConfig:
+    def __init__(self, regex, channel_id):
+        self.regex = regex
+        if channel_id == 'me':
+            self.channel_id = channel_id
+        else:
+            self.channel_id = int(channel_id)
+    
+    def __str__(self):
+        return str(self.channel_id) + ' ' + self.regex
 
 class Config:
     def __init__(self):        
@@ -7,7 +19,7 @@ class Config:
         self.block_outgoing = config('BLOCK_OUTGOING', default=False) #не пересылать собственные сообщения если True
 
         tmp_sources = config('SOURCE_CHANNELS').split(',')
-        
+
         self.sources_ids = []
         for source in tmp_sources:
             if source == 'me':
@@ -15,8 +27,22 @@ class Config:
             else:
                 self.sources_ids.append(int(source))
 
-        self.copy_to_channel = config('COPY_TO_CHANNEL')
-        if self.copy_to_channel != 'me':
-            self.copy_to_channel = int(self.copy_to_channel)
+        self.channels = []
+        for i in range(config('MAX_REGEX', cast=int, default=10)):
+            regex = config('REGEX_' + str(i), default=None)
+            copy_to_channel = config('COPY_TO_CHANNEL_' + str(i), default=None)
+            if regex and copy_to_channel:
+                new_channel = ChannelConfig(regex, copy_to_channel)
+                self.channels.append(new_channel)
+                print('channel', str(i), 'config:', new_channel)
+
+        logging_level = config('LOGGING_LEVEL', default='ERROR')
+
+        if logging_level == 'DEBUG':
+            self.logging_level = logging.ERROR
+        elif logging_level == 'INFO':
+            self.logging_level = logging.ERROR
+        else: 
+            self.logging_level = logging.ERROR
 
 conf = Config()
--- index.py
+++ index.py
@@ -1,26 +1,46 @@
+import re
+import logging
+import json 
+from typing import List
 from pyrogram import Client, filters
 from pyrogram.handlers import MessageHandler
 from pyrogram.types import Message
-from config import conf
-from typing import List
+from config import conf, ChannelConfig
 
-def forward_content(source_channel_ids: List[int], copy_to_channel_id: int):
+logger = logging.getLogger("tggt")
+logger.setLevel(conf.logging_level)
+fh = logging.FileHandler("logs/app.log")
+logger.addHandler(fh)
+
+def forward_content(source_channel_ids: List[int], channels: List[ChannelConfig]):
     """
         Функция прослушивает новые сообщения из каналов source_channel_ids
-        и отправляет их в copy_to_channel_id
+        и отправляет их в channels, если совпадает регулярка
     """
 
     client = Client(name='app_client', api_id=conf.api_id, api_hash=conf.api_hash)
         
     def forward_message(client: Client, message: Message):
         """
-            Отправка сообщения в свой канал
+            Отправка сообщения в свои каналы
         """
 
         print('new message: "', message.text, '"')
-        message.forward(chat_id=copy_to_channel_id)
-        if message.link:
-            client.send_message(chat_id=copy_to_channel_id, text=message.link)
+        logger.info('new message')
+        logger.info(str(message))
+        message_to_check = message.text or message.caption
+        if not message_to_check:
+            return
+        
+        for channel in channels:            
+            if re.search(channel.regex, message_to_check):
+                logger.info('found match, sending to ' + str(channel.channel_id))
+                print('found match, sending to ' + str(channel.channel_id))
+                message.forward(chat_id=channel.channel_id)
+                if message.link:
+                    client.send_message(chat_id=channel.channel_id, text=message.link)
+            else:
+                logger.info('message', message_to_check, 'doesnt match with ', channel.regex)
 
     def filter_channels(self, client, message: Message):
         """
@@ -32,7 +52,6 @@ def forward_content(source_channel_ids: List[int], copy_to_channel_id: int):
             if message.outgoing:
                 return False
         # Пока пересылать всё из интересующих каналов
-        print(message)
         return message.chat.id in source_channel_ids
 
     filter_data = filters.create(filter_channels)
@@ -41,5 +60,5 @@ def forward_content(source_channel_ids: List[int], copy_to_channel_id: int):
     client.run()
     
 if __name__ == '__main__':
-    forward_content(conf.sources_ids, conf.copy_to_channel)
+    forward_content(conf.sources_ids, conf.channels)
 
